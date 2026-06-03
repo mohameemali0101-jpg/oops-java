87810d2b9acc8c4eb1d45fc262f04b496ac7520f
mohameemali0101-jpg <mohameemali0101@gmail.com>
2026-06-03 11:29:51 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,11 @@
+package com.tcs.prep;
+
+public class employee {
+
+	public employee() {
+		System.out.println("constructor called");
+	}
+    public static void main (String[] args) {
+    	employee em = new employee();
+    }
+}
